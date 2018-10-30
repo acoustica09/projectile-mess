b5ae4c5adfc2ad34fd2ef57d1ead2801183288c9
SPOmoe <43919261+SPOmoe@users.noreply.github.com>
2018-10-29 18:09:50 -0700

    Add files via upload

---
+++ AWorld.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * This world is a mess or throwable objects
+ * 
+ * @author Marlene Inoue
+ * @version 1.0 29 October 2018
+ */
+public class AWorld extends World {
+    public static final Vector GRAVITY = new Vector(0.0, 0.5);
+    
+    /**
+     * Constructor for objects of class AWorld.
+     * 
+     */
+    public AWorld() {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+    }
+}
